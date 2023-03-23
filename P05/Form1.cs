2d57e1eb09737cf5906c0a5e848f04dda9cfb57c
novotnyv.03 <novotnyv.03@A222-14>
2023-03-23 09:23:53 +0100

    P05 done

--- P05/Form1.cs
+++ P05/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,38 +24,38 @@ namespace P05
         private bool JeRodneCislo(string datum, out DateTime datum_z_rc, out string zprava)
         {
             
-            zprava = string.Format("Správné rodné číslo");
-            string rok = datum.Substring(0, 2);
-            string mesic = datum.Substring(2, 2);
-            string den = datum.Substring(4, 2);
-            datum = datum.Remove(6, 1);
-            long cislo = Int64.Parse(datum);
-            datum_z_rc = DateTime.Now;
-            if (cislo % 11 != 0)
+                zprava = string.Format("Správné rodné číslo");
+                string rok = datum.Substring(0, 2);
+                string mesic = datum.Substring(2, 2);
+                string den = datum.Substring(4, 2);
+                datum = datum.Remove(6, 1);
+                long cislo = Int64.Parse(datum);
+                datum_z_rc = DateTime.Now;
+            try
             {
-                zprava = string.Format("Toto není rodné číslo, protože není dělitelné číslem 11");
+
+                if (cislo % 11 != 0) throw new FormatException();           
+                int rokc = Int32.Parse(rok); //Zjistit rok, jestli 2003 nebo 1908
+                int mesc = Int32.Parse(mesic);
+                int denc = Int32.Parse(den);
+                if (mesc > 50)
+                {
+                    mesc -= 50;
+                }
+                if (mesc < 1 || mesc > 12)
+                {
+                    zprava = String.Format("Špatný měsíc");
+                    return false;
+                }
+                int pocetdni = DateTime.DaysInMonth(rokc, mesc);
+                if (denc < 1 || denc > pocetdni) throw new FormatException();
+                datum_z_rc = new DateTime(rokc + 2000, mesc, denc);
+                return true;
+            }catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
                 return false;
             }
-            int rokc = Int32.Parse(rok); //Zjistit rok, jestli 2003 nebo 1908
-            int mesc = Int32.Parse(mesic);
-            int denc = Int32.Parse(den);
-            if (mesc > 50)
-            {
-                mesc -= 50;
-            }
-            if (mesc < 1 || mesc > 12)
-            {
-                zprava = String.Format("Špatný měsíc");
-                return false;
-            }
-            int pocetdni = DateTime.DaysInMonth(rokc, mesc);
-            if (denc < 1 || denc > pocetdni)
-            {
-                zprava = String.Format("Špatný den!");
-                return false;
-            }
-            datum_z_rc = new DateTime(rokc + 2000, mesc, denc);
-            return true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -63,10 +64,6 @@ namespace P05
             if (JeRodneCislo(rc, out DateTime datum_z_rc, out string zprava))
             {
                 MessageBox.Show(string.Format($"Rodné číslo je {rc}, a převedení na datum je to {datum_z_rc:d}"));
-            }
-            else
-            {
-                MessageBox.Show("Rodne cislo je spatne");
             }
         }
     }
